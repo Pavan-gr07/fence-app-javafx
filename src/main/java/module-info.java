7497215b1bed095584a2610344d05c48f78b698e
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2025-05-14 15:54:40 +0530

    fence app http updated

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.fencedeviceapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires java.xml;
-    opens com.example.fencedeviceapp to javafx.fxml;
-    exports com.example.fencedeviceapp;
-}

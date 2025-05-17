d20d37caf6ade59f9d066071647467a74a74f551
Pavan G R <115134463+Pavan-gr07@users.noreply.github.com>
2025-05-17 17:08:07 +0530

    logo and all updated

--- src/main/java/com/example/fencedeviceapp/Main.java
+++ src/main/java/com/example/fencedeviceapp/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,9 +13,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("layout.fxml"));
-
         primaryStage.setScene(new Scene(loader.load()));
         primaryStage.setTitle("Device Status Monitor");
+
+        // Set the window icon
+        Image icon = new Image(Main.class.getResourceAsStream("Crown_logo.png"));
+        primaryStage.getIcons().add(icon);
 
         // Get controller reference
         controller = loader.getController();
@@ -27,33 +31,6 @@ public class Main extends Application {
         });
 
         primaryStage.show();
-
-        // If you want to load mock data for testing purposes,
-        // uncomment the following code block:
-        /*
-        String mockXml = """
-            <Response>
-              <FVout>87</FVout>
-              <FVReturn>87</FVReturn>
-              <BV>0</BV>
-              <SPV>0</SPV>
-              <ACStatus>0</ACStatus>
-              <SyncStatus>0</SyncStatus>
-              <LidAlarm>0</LidAlarm>
-              <EncloserAlarm>0</EncloserAlarm>
-              <DrainageIntrusionAlarm>1</DrainageIntrusionAlarm>
-              <TemparatureAlarm>0</TemparatureAlarm>
-              <IntrusionAlarm>0</IntrusionAlarm>
-              <FenceStaus>1</FenceStaus>
-              <LightStaus>0</LightStaus>
-              <ServiceMode>0</ServiceMode>
-              <Inp1>0</Inp1>
-              <Inp2>1</Inp2>
-              <Inp3>0</Inp3>
-            </Response>
-        """;
-        controller.updateStatusFromResponse(mockXml);
-        */
     }
 
     @Override
